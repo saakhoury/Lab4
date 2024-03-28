f8aa4a296fe0449f3043fff258ac235da3f6a372
Sanskriti Akhoury <117190841+saakhoury@users.noreply.github.com>
2024-03-28 13:26:13 -0400

    Update holding_register.vhd

--- holding_register.vhd
+++ holding_register.vhd
@@ -4,11 +4,11 @@ use ieee.std_logic_1164.all;
 
 entity holding_register is port (
 
-			clk					: in std_logic;
-			reset				: in std_logic;
+			clk			: in std_logic;
+			reset			: in std_logic;
 			register_clr		: in std_logic;
-			din					: in std_logic;
-			dout				: out std_logic
+			din			: in std_logic;
+			dout			: out std_logic
   );
  end holding_register;
  

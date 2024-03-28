a8cca711450fb7a3dc592ad245935e1284b2029d
Sanskriti Akhoury <117190841+saakhoury@users.noreply.github.com>
2024-03-28 13:44:55 -0400

    Update Clock_generator.vhd

--- Clock_generator.vhd
+++ Clock_generator.vhd
@@ -12,7 +12,7 @@ entity Clock_generator is
 	port
 	(
 		 sim_mode			: in boolean;	-- used to select the clocking frequency for the output signals "sm_clken" and "blink".
-		 reset				: in std_logic;
+		 reset				: in std_logic; -- reset input signal
        		 clkin      			: in std_logic; -- input used for counter and register clocking
 		 sm_clken			: out std_logic; -- output used to enbl the sm to advance by 1 clk.
 		 blink		  		: out std_logic  -- output used for blink signal (1/4 the rate of the sm_clken)
@@ -22,9 +22,9 @@ end entity;
 
 architecture rtl of Clock_generator is
 
-signal digital_counter 					: std_logic_vector(24 downto 0);
-signal clk_1hz, clk_4hz					: std_logic;
-signal sim_clk_blink, sim_clk_enbl			: std_logic;
+signal digital_counter 					: std_logic_vector(24 downto 0); -- counter that generates clock signals
+signal clk_1hz, clk_4hz					: std_logic; -- approximate 1hz and 4hz clk signals
+signal sim_clk_blink, sim_clk_enbl			: std_logic; -- clk signals for simulation
 
 signal clk_reg_extend					: std_logic_vector(1 downto 0); -- pipeline used to create a single clock enable pulse signal
 signal blink_sig 					: std_logic;
@@ -35,19 +35,19 @@ begin
 -- clk_divider process generates a free-running 1Hz Clock_enbl signal and a 4hz blink signal from the 50 Mhz clk
 
 clk_divider: process (clkin)
-	variable   counter	:  unsigned(24 downto 0);
+	variable   counter	:  unsigned(24 downto 0); -- counter variable that divides clock
 	
 	begin
 -- Synchronously update counter
 		if (rising_edge(clkin))  then
 			if(reset ='1') then
-				counter := "0000000000000000000000000";
+				counter := "0000000000000000000000000"; -- reset counter on reset signal
 			else
 				 counter :=  counter + 1;
 			end if;
 		end if;
 		
-		digital_counter <= std_logic_vector(counter);		
+		digital_counter <= std_logic_vector(counter); -- output counter value		
 
 	end process;
 	
@@ -60,8 +60,8 @@ clk_extender: process (clkin)	        -- clk_extender is an extra pair of serial
 	begin
 		if (rising_edge(clkin))  then
 			if(reset ='1') then
-				clk_reg_extend(1 downto 0) 	<= "00";
-				blink_sig 			<= '0';
+				clk_reg_extend(1 downto 0) 	<= "00"; -- reset clk_reg_extend on reset signal
+				blink_sig 			<= '0'; -- reset bling signal
 			elsif(sim_mode) then
 				 clk_reg_extend(1 downto 0) 	<= clk_reg_extend(0) & sim_clk_enbl; -- clock register input signal used for simulations
 				 blink_sig 		        <= sim_clk_blink;		      -- blink signal input used for blinking in simulations
@@ -72,7 +72,7 @@ clk_extender: process (clkin)	        -- clk_extender is an extra pair of serial
 		end if;
 	end process;
 
-sm_clken	<= clk_reg_extend(0) AND (NOT(clk_reg_extend(1)));
-blink 		<= blink_sig;
+sm_clken	<= clk_reg_extend(0) AND (NOT(clk_reg_extend(1))); -- output clock enable signal for state machine 
+blink 		<= blink_sig; -- output blink signal
 
 end rtl;

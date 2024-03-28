ed1cfaa51daf8124a24b8f5480d87438f43a02ff
Sanskriti Akhoury <117190841+saakhoury@users.noreply.github.com>
2024-03-28 13:19:37 -0400

    Update Clock_generator.vhd

--- Clock_generator.vhd
+++ Clock_generator.vhd
@@ -11,10 +11,10 @@ entity Clock_generator is
 
 	port
 	(
-		 sim_mode			: in boolean;		-- used to select the clocking frequency for the output signals "sm_clken" and "blink".
+		 sim_mode			: in boolean;	-- used to select the clocking frequency for the output signals "sm_clken" and "blink".
 		 reset				: in std_logic;
-       clkin      		: in  std_logic; -- input used for counter and register clocking
-		 sm_clken			: out	std_logic; -- output used to enbl the sm to advance by 1 clk.
+       		 clkin      			: in std_logic; -- input used for counter and register clocking
+		 sm_clken			: out std_logic; -- output used to enbl the sm to advance by 1 clk.
 		 blink		  		: out std_logic  -- output used for blink signal (1/4 the rate of the sm_clken)
 	);
 
@@ -24,10 +24,10 @@ architecture rtl of Clock_generator is
 
 signal digital_counter 					: std_logic_vector(24 downto 0);
 signal clk_1hz, clk_4hz					: std_logic;
-signal sim_clk_blink, sim_clk_enbl	: std_logic;
+signal sim_clk_blink, sim_clk_enbl			: std_logic;
 
 signal clk_reg_extend					: std_logic_vector(1 downto 0); -- pipeline used to create a single clock enable pulse signal
-signal blink_sig 							: std_logic;
+signal blink_sig 					: std_logic;
 
 
 begin
@@ -53,26 +53,26 @@ clk_divider: process (clkin)
 	
 clk_1hz 				<= digital_counter(24); -- clk_1Hz is approximate
 clk_4hz 				<= digital_counter(22); -- clk_4Hz is approximate
-sim_clk_enbl 		<= digital_counter(4);  -- clk_enbl used in simulations
-sim_clk_blink 		<= digital_counter(2);  -- clk_blink used in simulations
+sim_clk_enbl 				<= digital_counter(4);  -- clk_enbl used in simulations
+sim_clk_blink 				<= digital_counter(2);  -- clk_blink used in simulations
 
-clk_extender: process (clkin)				   -- clk_extender is an extra pair of serial registers used to create a single one-cycle enable pulse for the state machine
+clk_extender: process (clkin)	        -- clk_extender is an extra pair of serial registers used to create a single one-cycle enable pulse for the state machine
 	begin
 		if (rising_edge(clkin))  then
 			if(reset ='1') then
 				clk_reg_extend(1 downto 0) 	<= "00";
-				blink_sig 				 			<= '0';
+				blink_sig 			<= '0';
 			elsif(sim_mode) then
-				 clk_reg_extend(1 downto 0) 	<= clk_reg_extend(0) & sim_clk_enbl;-- clock register input signal used for simulations
-				 blink_sig 		 					<= sim_clk_blink;							-- blink signal input used for blinking in simulations
+				 clk_reg_extend(1 downto 0) 	<= clk_reg_extend(0) & sim_clk_enbl; -- clock register input signal used for simulations
+				 blink_sig 		        <= sim_clk_blink;		      -- blink signal input used for blinking in simulations
 			else 
 				 clk_reg_extend(1 downto 0) 	<= clk_reg_extend(0) & clk_1hz ; 	-- clock register input signal used for 1Hz clock enbl for LogicalStep Board downloads
-				 blink_sig 				 			<= clk_4hz;           					-- blink signal input used for LogicalStep Board downloads
+				 blink_sig 			<= clk_4hz;           			-- blink signal input used for LogicalStep Board downloads
 				END IF;
 		end if;
 	end process;
 
 sm_clken	<= clk_reg_extend(0) AND (NOT(clk_reg_extend(1)));
-blink 	<= blink_sig;
+blink 		<= blink_sig;
 
 end rtl;

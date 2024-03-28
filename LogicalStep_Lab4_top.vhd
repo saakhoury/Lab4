3139c0543f86a68b14f97665d165e521148c84d2
Sanskriti Akhoury <117190841+saakhoury@users.noreply.github.com>
2024-03-28 16:03:01 -0400

    Update LogicalStep_Lab4_top.vhd

--- LogicalStep_Lab4_top.vhd
+++ LogicalStep_Lab4_top.vhd
@@ -19,7 +19,9 @@ ENTITY LogicalStep_Lab4_top IS
    seg7_char1  	: out	std_logic;		    -- seg7 digi selectors
    seg7_char2  	: out	std_logic		    -- seg7 digi selectors
 	);
-	
+
+   -- outputs for the simulation 
+   -- sm_clken, blink_sig, NS_red, NS_green, NS_amber, EW_red, EW_green, EW_amber : out std_logic 
 END LogicalStep_Lab4_top;
 
 ARCHITECTURE SimpleCircuit OF LogicalStep_Lab4_top IS
@@ -83,15 +85,16 @@ ARCHITECTURE SimpleCircuit OF LogicalStep_Lab4_top IS
   end component;		
  
 ----------------------------------------------------------------------------------------------------
-	CONSTANT sim_mode								: boolean := FALSE;  -- set to FALSE for LogicalStep board downloads																						-- set to TRUE for SIMULATIONS
-	SIGNAL rst, rst_n_filtered, sync_rst						: std_logic;
+	CONSTANT sim_mode								: boolean := FALSE;  -- set to FALSE for LogicalStep board downloads	
+													     -- set to TRUE for SIMULATIONS
+	SIGNAL rst, rst_n_filtered, sync_rst						: std_logic; -- reset for registers
 	SIGNAL sm_clken, blink_sig							: std_logic; 
 	SIGNAL pb_n_filtered, pb							: std_logic_vector(3 downto 0); 
-	SIGNAL NS_red, NS_green, NS_amber, EW_red, EW_green, EW_amber  			: std_logic;
-	SIGNAL NS, EW									: std_logic := '0';
-	SIGNAL sync_input_NS, sync_input_EW						: std_logic;
-	SIGNAL NS_clear, EW_clear 							: std_logic := '0';
-	SIGNAL NS_7segment, EW_7segment							: std_logic_vector(6 downto 0);
+	SIGNAL NS_red, NS_green, NS_amber, EW_red, EW_green, EW_amber  			: std_logic;			-- signal outputs
+	SIGNAL NS, EW									: std_logic := '0';		-- state machine holds EW/NS requests until processed
+	SIGNAL sync_input_NS, sync_input_EW						: std_logic;			-- button input once its synchronized with clk 
+	SIGNAL NS_clear, EW_clear 							: std_logic := '0';		-- after request is processed, send clear signals to clear the hold registers
+	SIGNAL NS_7segment, EW_7segment							: std_logic_vector(6 downto 0); -- 7 bit value to be sent to the output of the 7 segment traffic lights display
 	
 BEGIN
 ----------------------------------------------------------------------------------------------------
@@ -100,20 +103,24 @@ INST1: pb_inverters	 port map (rst_n_filtered, rst, pb_n_filtered, pb);
 INST2: synchronizer      port map (clkin_50,sync_rst, rst, sync_rst);	-- the synchronizer is also reset by synch_rst.
 INST3: clock_generator 	 port map (sim_mode, sync_rst, clkin_50, sm_clken, blink_sig);
 
+-- synchronizes button inputs 
 INST4: synchronizer	 port map (clkin_50, sync_rst, pb(0), sync_input_NS);
 INST5: synchronizer 	 port map (clkin_50, sync_rst, pb(1), sync_input_EW);
 
- INST6: holding_register port map (clkin_50, rst, NS_clear, sync_input_NS, NS);
- INST7: holding_register port map (clkin_50, rst, EW_clear, sync_input_EW, EW);
+-- button inputs are proccessed and held as requests until cleared
+INST6: holding_register port map (clkin_50, rst, NS_clear, sync_input_NS, NS);
+INST7: holding_register port map (clkin_50, rst, EW_clear, sync_input_EW, EW);
 
 --INST6: holding_register port map (clkin_50, sync_rst, '0', sync_input_NS, NS);
-
+	
+-- functionality is to send requests and enable clock to the state machine for processing, lights and signals are outputs to be displayed, clears the hold registers after processing
 INST8: State_Machine port map (clkin_50, sync_rst, sm_clken, blink_sig, NS, EW, leds(7 downto 4), NS_clear, leds(0), NS_amber, NS_green, NS_red, EW_clear, leds(2), EW_amber, EW_green, EW_red);
---
+
 --INST7: holding_register port map (clkin_50, sync_rst, '0', sync_input_EW, EW);
 
- NS_7segment (6 downto 0) <= NS_amber & "00" & NS_green & "00" & NS_red;
- EW_7segment (6 downto 0) <= EW_amber & "00" & EW_green & "00" & EW_red;
+-- for the 7 segment display concatenate outputs into 7 bit values 
+NS_7segment (6 downto 0) <= NS_amber & "00" & NS_green & "00" & NS_red;
+EW_7segment (6 downto 0) <= EW_amber & "00" & EW_green & "00" & EW_red;
  
 INST9: segment7_mux 	port map (clkin_50, NS_7segment, EW_7segment, seg7_data, seg7_char2, seg7_char1);
 

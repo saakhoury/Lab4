667200353a0b5b4e455365de1cd836451472212b
Sanskriti Akhoury <117190841+saakhoury@users.noreply.github.com>
2024-03-28 13:22:41 -0400

    Update PB_inverters.vhd

--- PB_inverters.vhd
+++ PB_inverters.vhd
@@ -5,9 +5,9 @@ use ieee.std_logic_1164.all;
 entity PB_inverters is port (
 	rst_n				: in	std_logic;
 	rst				: out std_logic;
- 	pb_n_filtered	: in  std_logic_vector (3 downto 0);
-	pb					: out	std_logic_vector(3 downto 0)							 
-	); 
+ 	pb_n_filtered			: in  std_logic_vector (3 downto 0);
+	pb				: out	std_logic_vector(3 downto 0)							 
+); 
 end PB_inverters;
 
 architecture ckt of PB_inverters is
